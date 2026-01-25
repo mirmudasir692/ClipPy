56838631f979c71e5c3565d908c3c05fd241ac90
mirmudasir692 <mirmudasir692@gmail.com>
2026-01-25 22:40:15 +0530

    feat(core): implement video merging and image overlay functionality

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-from clippy import get_audio_from_video, merge_videos
+from clippy import get_audio_from_video, merge_videos, composite_image_over_video
 
-merge_videos("7. Scaling Databases.mp4", "13. Caching at different levels.mp4", "MergedVideo.mp4")
+composite_image_over_video(
+    video_path="vid.mp4",
+    image_path="logo.png",
+    start=20,
+    opacity=0.3,
+    position="center"
+)
